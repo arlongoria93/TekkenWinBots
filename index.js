2e0bad30a99e0371865d16ab8be29b9901124c41
Angelo Longoria <arlongoria93@gmail.com>
2021-04-07 18:24:35 -0700

    working on displaying wins in discord

--- index.js
+++ index.js
@@ -20,30 +20,44 @@ client.on("ready", () => {
 // add w @player
 // add 5 w @player
 // add(5,w)
-const printTotalWins = (mention) => {
-  console.log(mention);
+const printTotalWins = (mention, msg) => {
+  let ids = [];
+  mention.users.each((user) => {
+    ids.push(user.id);
+  });
+  const text =
+    "SELECT * FROM records WHERE (player1_id=$1 OR player1_id=$2) AND (player2_id=$2 OR player2_id=$1)";
+  const values = [ids[0], ids[1]];
+  clientdb.query(text, values, (err, res) => {
+    // console.log(res.rows[0].player1_wins, res.rows[0].player2_wins);
+    msg.channel.send(
+      `<@${res.rows[0].player1_id}> W: ${res.rows[0].player1_wins} <@${res.rows[0].player2_id}> W: ${res.rows[0].player2_wins}`
+    );
+    if (err) console.log(err);
+  });
 };
 let player1Wins = 100;
 client.on("message", (msg) => {
   if (!msg.content.startsWith("!") || msg.author.bot) return;
 
   let message = msg.content;
-  let splitMessage = message.split(" ");
-  splitMessage.shift();
-  if (splitMessage[0] === "wins") {
+  const args = message.slice(1).trim().split(" ");
+  const commandName = args.shift().toLowerCase();
+
+  if (commandName === "wins") {
     if (msg.mentions.length > 1) return "err";
     //mention === msg.mentions
-    printTotalWins(msg.mentions);
-  } else if (splitMessage[0] === "add") {
+    printTotalWins(msg.mentions, msg);
+  } else if (commandName === "add") {
   }
 
   if (msg.content === "test") {
     const text =
       "INSERT INTO records(player1_id,player2_id,player1_wins,player2_wins) VALUES ($1,$2,$3,$4)";
     const values = [];
-    // clientdb.query(text, values, (err, res) => {
-    //   if (err) console.log(err);
-    // });
+    clientdb.query(text, values, (err, res) => {
+      if (err) console.log(err);
+    });
   }
 });
 
