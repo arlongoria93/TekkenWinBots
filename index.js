834a9dd33c019e6e19c795db0852ace4594fb8f1
Angelo Longoria <arlongoria93@gmail.com>
2021-04-07 17:28:58 -0700

    commit

--- index.js
+++ index.js
@@ -5,31 +5,45 @@ const { Client } = require("pg");
 const clientdb = new Client({
   user: "angelo",
   host: "localhost",
-  database: "test",
+  database: "tekkenwins",
   password: "123",
   port: "5432",
 });
 clientdb.connect();
-clientdb.query("SELECT * from person", (err, res) => {
-  console.log(err ? err.stack : res.rows[0]); // Hello World!
-  clientdb.end();
-});
 
 client.on("ready", () => {
   console.log(`Hello World!`);
 });
 
+//wins
+// wins @player
+// add w @player
+// add 5 w @player
+// add(5,w)
+const printTotalWins = (mention) => {
+  console.log(mention);
+};
+let player1Wins = 100;
 client.on("message", (msg) => {
-  if (msg.author.bot) return;
+  if (!msg.content.startsWith("!") || msg.author.bot) return;
 
-  console.log(msg.mentions);
-  msg.mentions.users.each((user) => {
-    console.log(user.id);
-  });
-  if (msg.content === "!wins" || msg.content === "!Wins") {
-    msg.reply("Pong!");
-  } else {
-    msg.channel.send("Please type either !wins or !Wins");
+  let message = msg.content;
+  let splitMessage = message.split(" ");
+  splitMessage.shift();
+  if (splitMessage[0] === "wins") {
+    if (msg.mentions.length > 1) return "err";
+    //mention === msg.mentions
+    printTotalWins(msg.mentions);
+  } else if (splitMessage[0] === "add") {
+  }
+
+  if (msg.content === "test") {
+    const text =
+      "INSERT INTO records(player1_id,player2_id,player1_wins,player2_wins) VALUES ($1,$2,$3,$4)";
+    const values = [];
+    // clientdb.query(text, values, (err, res) => {
+    //   if (err) console.log(err);
+    // });
   }
 });
 
