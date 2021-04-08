d7ca0fd480782f171eca88b24d6fe2767b55ecd6
Angelo Longoria <arlongoria93@gmail.com>
2021-04-07 18:36:34 -0700

    finished printing total wins function

--- index.js
+++ index.js
@@ -25,16 +25,37 @@ const printTotalWins = (mention, msg) => {
   mention.users.each((user) => {
     ids.push(user.id);
   });
-  const text =
-    "SELECT * FROM records WHERE (player1_id=$1 OR player1_id=$2) AND (player2_id=$2 OR player2_id=$1)";
-  const values = [ids[0], ids[1]];
-  clientdb.query(text, values, (err, res) => {
-    // console.log(res.rows[0].player1_wins, res.rows[0].player2_wins);
-    msg.channel.send(
-      `<@${res.rows[0].player1_id}> W: ${res.rows[0].player1_wins} <@${res.rows[0].player2_id}> W: ${res.rows[0].player2_wins}`
-    );
-    if (err) console.log(err);
-  });
+  if (ids.length === 2) {
+    const text =
+      "SELECT * FROM records WHERE (player1_id=$1 OR player1_id=$2) AND (player2_id=$2 OR player2_id=$1)";
+
+    const values = [ids[0]];
+    clientdb.query(text, values, (err, res) => {
+      // console.log(res.rows[0].player1_wins, res.rows[0].player2_wins);
+      msg.channel.send(
+        `<@${res.rows[0].player1_id}> W: ${res.rows[0].player1_wins} <@${res.rows[0].player2_id}> W: ${res.rows[0].player2_wins}`
+      );
+      if (err) console.log(err);
+    });
+  }
+  if (ids.length === 1) {
+    const text = "SELECT * FROM records WHERE (player1_id=$1 OR player2_id=$1)";
+    const values = [ids[0]];
+    clientdb.query(text, values, (err, res) => {
+      // console.log(res.rows[0].player1_wins, res.rows[0].player2_wins);
+      let totalWins = 0;
+      res.rows.forEach((row) => {
+        if (row.player1_id == ids[0]) {
+          totalWins += row.player1_wins;
+        } else {
+          totalWins += row.player2_wins;
+        }
+      });
+      msg.channel.send(`W: ${totalWins}`);
+
+      if (err) console.log(err);
+    });
+  }
 };
 let player1Wins = 100;
 client.on("message", (msg) => {
